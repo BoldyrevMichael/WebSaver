2c49938b7eb182aa4f4a68ebc28ec71a2c1c2f7d
BoldyrevMichael <mmmsnddd@icloud.com>
2018-01-07 16:49:01 +0300

    Продолжение построения ГПИ клиента. Принято решение обработку событий выполнять методами отдельного класса (слушателя), чтобы не загромождать класс clientGUI.

--- client/src/main/java/ru/geekbrains/websaver/client/ClientController.java
+++ client/src/main/java/ru/geekbrains/websaver/client/ClientController.java
@@ -1,4 +1,40 @@
 package ru.geekbrains.websaver.client;
 
-public class ClientController {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClientController implements ActionListener {
+
+    ClientGUI clientGUI;
+
+    public void addClient(ClientGUI clientGUI) {
+        this.clientGUI = clientGUI;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object src = e.getSource();
+        if (src == clientGUI.btnEntrance) {
+            register();
+        } else if (src == clientGUI.btnEntrance) {
+            login();
+        } else {
+            throw new RuntimeException("Unknown src = " + src);
+        }
+    }
+
+    private void register() {
+        SwingUtilities.invokeLater(() -> {
+            clientGUI.welcomePanel.setVisible(false);
+            clientGUI.registrPanel.setVisible(true);
+        });
+    }
+
+    private void login() {
+        SwingUtilities.invokeLater(() -> {
+            clientGUI.welcomePanel.setVisible(false);
+            clientGUI.entrancePanel.setVisible(true);
+        });
+    }
 }
--- client/src/main/java/ru/geekbrains/websaver/client/ClientGUI.java
+++ client/src/main/java/ru/geekbrains/websaver/client/ClientGUI.java
@@ -1,20 +1,17 @@
 package ru.geekbrains.websaver.client;
 
 import ru.geekbrains.websaver.common.DefaultGUIExceptionHandler;
+import ru.geekbrains.websaver.common.GBC;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-public class ClientGUI extends JFrame implements ActionListener {
+public class ClientGUI extends JFrame {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new ClientGUI();
-            }
+        ClientController clientController = new ClientController();
+        SwingUtilities.invokeLater(() -> {
+            new ClientGUI(clientController);
         });
     }
 
@@ -22,76 +19,88 @@ public class ClientGUI extends JFrame implements ActionListener {
     private static final int HEIGHT = 600;
     private static final String TITLE = "WebSaver";
 
-    private final JPanel welcomePanel = new JPanel(new BorderLayout());
-    private final JButton btnEntrance = new JButton("Войти");
-    private final JButton btnRegistr = new JButton("Зарегистрироваться");
-    private final JLabel labWelcome = new JLabel("Welcome to WebSaver!!!");
+    final JPanel welcomePanel = new JPanel(new BorderLayout(0, 200));
+    private final JPanel welcomePanelbottonPanel = new JPanel(new FlowLayout());
+    final JButton btnEntrance = new JButton("Войти");
+    final JButton btnRegistr = new JButton("Зарегистрироваться");
+    private final JLabel labWelcome = new JLabel("Welcome to WebSaver!!!", JLabel.CENTER);
 
-    private final JPanel registrPanel = new JPanel(new GridLayout(3, 2));
-    private final JLabel labRegLogin = new JLabel("Придумайте логин: ");
-    private final JTextField fieldRegLogin = new JTextField("логин");
-    private final JLabel labRegPass = new JLabel("Придумайте пароль: ");
-    private final JPasswordField fieldRegPass = new JPasswordField("пароль");
-    private final JLabel labRegRepPass = new JLabel("Повторите пароль: ");
-    private final JPasswordField fieldRegRepPass = new JPasswordField("пароль повторно");
+    final JPanel registrPanel = new JPanel(new GridBagLayout());
+    private final JLabel labRegLogin = new JLabel("Придумайте логин: ", JLabel.RIGHT);
+    final JTextField fieldRegLogin = new JTextField("логин");
+    private final JLabel labRegPass = new JLabel("Придумайте пароль: ", JLabel.RIGHT);
+    final JPasswordField fieldRegPass = new JPasswordField("пароль");
+    private final JLabel labRegRepPass = new JLabel("Повторите пароль: ", JLabel.RIGHT);
+    final JPasswordField fieldRegRepPass = new JPasswordField("пароль");
 
-    private final JPanel entrancePanel = new JPanel(new GridLayout(2, 2));
-    private final JLabel labEntrLogin = new JLabel("Введите логин: ");
-    private final JTextField fieldEntrLogin = new JTextField("логин");
-    private final JLabel labEntrPass = new JLabel("Введите пароль: ");
-    private final JPasswordField fieldEntrPass = new JPasswordField("пароль");
+    final JPanel entrancePanel = new JPanel(new GridBagLayout());
+    private final JLabel labEntrLogin = new JLabel("Введите логин: ", JLabel.RIGHT);
+    JTextField fieldEntrLogin = new JTextField("логин");
+    private final JLabel labEntrPass = new JLabel("Введите пароль: ", JLabel.RIGHT);
+    final JPasswordField fieldEntrPass = new JPasswordField("пароль");
 
-    private final JPanel bottonPanel = new JPanel(new GridLayout());
-    private final JButton btnAddFiles = new JButton("Добавить Файл(ы)");
-    private final JButton btnDelFiles = new JButton("Удалить Файл(ы)");
-    private final JButton btnSyncFiles = new JButton("Синхронизировать Файл(ы)");
+    final JPanel bottonPanel = new JPanel(new GridBagLayout());
+    final JButton btnAddFiles = new JButton("Добавить Файл(ы)");
+    final JButton btnDelFiles = new JButton("Удалить Файл(ы)");
+    final JButton btnSyncFiles = new JButton("Синхронизировать Файл(ы)");
 
-    private final JTable listOfFilesTable = new JTable();
+    final JTable listOfFilesTable = new JTable();
 
-    private ClientGUI() {
+    private ClientGUI(ClientController clientController) {
+        clientController.addClient(this);
         Thread.setDefaultUncaughtExceptionHandler(new DefaultGUIExceptionHandler());
         setLayout(new BorderLayout());
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(WIDTH, HEIGHT);
+        setMinimumSize(new Dimension(620, 400));
         setTitle(TITLE);
         setLocationRelativeTo(null);
 
-        registrPanel.add(labRegLogin);
-        registrPanel.add(fieldRegLogin);
-        registrPanel.add(labRegPass);
-        registrPanel.add(fieldRegPass);
-        registrPanel.add(labRegRepPass);
-        registrPanel.add(fieldRegRepPass);
+        bottonPanel.add(btnAddFiles, new GBC(0, 0).setFill(GBC.NONE).setAnchor(GBC.CENTER).setInsets(20));
+        bottonPanel.add(btnDelFiles, new GBC(0, 1).setFill(GBC.NONE).setAnchor(GBC.CENTER).setInsets(20));
+        bottonPanel.add(btnSyncFiles, new GBC(0, 2).setFill(GBC.NONE).setAnchor(GBC.CENTER).setInsets(20));
+        bottonPanel.setPreferredSize(new Dimension(200, 0));
+        bottonPanel.setVisible(false);
+        add(bottonPanel, BorderLayout.EAST);
+
+        JScrollPane scrollListOfFilesTable = new JScrollPane(listOfFilesTable);
+        scrollListOfFilesTable.setVisible(false);
+        add(listOfFilesTable, BorderLayout.CENTER);
+
+        registrPanel.add(labRegLogin, new GBC(0, 0).setFill(GBC.NONE).setAnchor(GBC.EAST).setInsets(10, 0, 10, 0));
+        fieldRegLogin.setPreferredSize(new Dimension(250, 30));
+        registrPanel.add(fieldRegLogin, new GBC(1, 0).setFill(GBC.NONE).setAnchor(GBC.WEST).setInsets(10, 0, 10, 0));
+        registrPanel.add(labRegPass, new GBC(0, 1).setFill(GBC.NONE).setAnchor(GBC.EAST).setInsets(10, 0, 10, 0));
+        fieldRegPass.setPreferredSize(new Dimension(250, 30));
+        registrPanel.add(fieldRegPass, new GBC(1, 1).setFill(GBC.NONE).setAnchor(GBC.WEST).setInsets(10, 0, 10, 0));
+        registrPanel.add(labRegRepPass, new GBC(0, 2).setFill(GBC.NONE).setAnchor(GBC.EAST).setInsets(10, 0, 10, 0));
+        fieldRegRepPass.setPreferredSize(new Dimension(250, 30));
+        registrPanel.add(fieldRegRepPass, new GBC(1, 2).setFill(GBC.NONE).setAnchor(GBC.WEST).setInsets(10, 0, 10, 0));
         registrPanel.setVisible(false);
         add(registrPanel);
 
-        entrancePanel.add(labEntrLogin);
-        entrancePanel.add(fieldEntrLogin);
-        entrancePanel.add(labEntrPass);
-        entrancePanel.add(fieldEntrPass);
+        entrancePanel.add(labEntrLogin, new GBC(0, 0).setFill(GBC.NONE).setAnchor(GBC.EAST).setInsets(10, 0, 10, 0));
+        fieldEntrLogin.setPreferredSize(new Dimension(250, 30));
+        entrancePanel.add(fieldEntrLogin, new GBC(1, 0).setFill(GBC.NONE).setAnchor(GBC.WEST).setInsets(10, 0, 10, 0));
+        entrancePanel.add(labEntrPass, new GBC(0, 1).setFill(GBC.NONE).setAnchor(GBC.EAST).setInsets(10, 0, 10, 0));
+        fieldEntrPass.setPreferredSize(new Dimension(250, 30));
+        entrancePanel.add(fieldEntrPass, new GBC(1, 1).setFill(GBC.NONE).setAnchor(GBC.WEST).setInsets(10, 0, 10, 0));
         entrancePanel.setVisible(false);
         add(entrancePanel);
 
-        bottonPanel.add(btnAddFiles);
-        bottonPanel.add(btnDelFiles);
-        bottonPanel.add(btnSyncFiles);
-        bottonPanel.setPreferredSize(new Dimension(150, 0));
-        bottonPanel.setVisible(false);
-        add(bottonPanel,BorderLayout.EAST);
-
-        listOfFilesTable.setVisible(false);
-        add(listOfFilesTable,BorderLayout.CENTER);
-
+        labWelcome.setFont(new Font("Dialog", Font.BOLD, 50));
         welcomePanel.add(labWelcome, BorderLayout.NORTH);
-        welcomePanel.add(btnEntrance, BorderLayout.WEST);
-        welcomePanel.add(btnRegistr, BorderLayout.EAST);
-        add(welcomePanel);
+        welcomePanelbottonPanel.add(btnEntrance);
+        welcomePanelbottonPanel.add(btnRegistr);
+        welcomePanel.add(welcomePanelbottonPanel, BorderLayout.CENTER);
+        add(welcomePanel, BorderLayout.CENTER);
+
+        btnRegistr.addActionListener(clientController);
+        btnEntrance.addActionListener(clientController);
+        btnAddFiles.addActionListener(clientController);
+        btnDelFiles.addActionListener(clientController);
+        btnSyncFiles.addActionListener(clientController);
 
         setVisible(true);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
     }
 }
