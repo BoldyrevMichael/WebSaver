2783d654d31c7d77aac7a3b4b90fe25e177eb913
BoldyrevMichael <mmmsnddd@icloud.com>
2018-01-08 20:14:00 +0300

    Начата реализация ServerCore, подготовлен метод для создания базы данных с информацией о клиентах.

--- server/src/main/java/ru/geekbrains/websaver/server/ServerCore.java
+++ server/src/main/java/ru/geekbrains/websaver/server/ServerCore.java
@@ -5,15 +5,47 @@ import ru.geekbrains.websaver.common.DataExchangeSocketThreadListener;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Vector;
 
 public class ServerCore implements ServerSocketThreadListener, DataExchangeSocketThreadListener {
 
+    private static Connection connection;
     private final Vector<DataExchangeSocketThread> clients = new Vector<>();
 
     public static void main(String[] args) {
         ServerCore serverCore = new ServerCore();
-        ServerSocketThread serverSocketThread = new ServerSocketThread("ServerSocketThread",8189,2000,serverCore);
+        initDB();
+        ServerSocketThread serverSocketThread = new ServerSocketThread("ServerSocketThread", 8189, 2000, serverCore);
+    }
+
+    private static void initDB() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:ClientsOfWebSaver.db");
+            Statement stmt = connection.createStatement();
+            stmt.execute("CREATE TABLE IF NOT EXISTS Clients (\n" +
+                    "    idOfClient      INTEGER PRIMARY KEY AUTOINCREMENT, \n" +
+                    "    login           TEXT UNIQUE ON CONFLICT FAIL,\n" +
+                    "    password        TEXT,\n" +
+                    "    volumeOfData    DECIMAL(14,2),\n" +
+                    "    numberOfFiles   INTEGER;");
+            stmt.execute("CREATE TABLE IF NOT EXISTS timeOfVisits (\n" +
+                    "    idOfVisit       INTEGER PRIMARY KEY AUTOINCREMENT, \n" +
+                    "    loginOfClient   INTEGER REFERENCES Clients(login) ON DELETE CASCADE,\n" +
+                    "    timeOfLastVisit INTEGER);");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void register() {
+
+
+
     }
 
     @Override
@@ -49,23 +81,27 @@ public class ServerCore implements ServerSocketThreadListener, DataExchangeSocke
     }
 
     @Override
-    public synchronized void onStartDataExchangeSocketThread(DataExchangeSocketThread dataExchangeSocketThread, Socket socket) {
+    public synchronized void onStartDataExchangeSocketThread(DataExchangeSocketThread
+                                                                     dataExchangeSocketThread, Socket socket) {
         System.out.println("На сервере начал работу поток для обмена данными с клиентом " + socket + "..");
     }
 
     @Override
-    public synchronized void onReadyDataExchangeSocketThread(DataExchangeSocketThread dataExchangeSocketThread, Socket socket) {
+    public synchronized void onReadyDataExchangeSocketThread(DataExchangeSocketThread
+                                                                     dataExchangeSocketThread, Socket socket) {
         System.out.println("На сервере поток для обмена данными с клиентом " + socket + " подготовлен к передаче данных..");
         clients.add(dataExchangeSocketThread);
     }
 
     @Override
-    public synchronized void onExceptionDataExchangeSocketThread(DataExchangeSocketThread dataExchangeSocketThread, Socket socket, Exception e) {
+    public synchronized void onExceptionDataExchangeSocketThread(DataExchangeSocketThread
+                                                                         dataExchangeSocketThread, Socket socket, Exception e) {
         System.out.println("Exception: " + e.getClass().getName() + ": " + e.getMessage());
     }
 
     @Override
-    public synchronized void onStopDataExchangeSocketThread(DataExchangeSocketThread dataExchangeSocketThread, Socket socket) {
+    public synchronized void onStopDataExchangeSocketThread(DataExchangeSocketThread
+                                                                    dataExchangeSocketThread, Socket socket) {
         System.out.println("На сервере завершил работу поток для обмена данными с клиентом " + socket + ".");
         clients.remove(dataExchangeSocketThread);
     }
